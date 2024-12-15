9b508b3f1e8fd418ccbf11c0c4e0eff817d860a3
baweiyang <baweiyang@meituan.com>
2024-12-15 22:09:39 +0800

    【功能新增】前后端项目初始化

---
+++ src/test/java/cn/bawy/mvp/netdisk/NetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bawy.mvp.netdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
